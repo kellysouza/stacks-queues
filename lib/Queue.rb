24c4d96ed6a75d3d71eaa18aab692e8fdc523698
Kelly Souza <kellysouza99@gmail.com>
2017-02-23 12:28:45 -0800

    defines queues methods, tests passing

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store.push(element)
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    return @store.first
   end
 
   def size
+    return @store.count
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
+    return @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,23 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
